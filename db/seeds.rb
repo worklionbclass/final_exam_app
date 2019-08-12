f8dcddb8c86f2454502e6133a8c1ffafa6602e75
Ubuntu <ubuntu@ip-172-31-44-43.ap-northeast-1.compute.internal>
2019-08-12 15:05:48 +0000

    set faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Faker::Config.locale = 'ko'
+
+4.times do
+    user = User.create(email: Faker::Internet.email, name: Faker::Name.name, password: "111111", password_confirmation: "111111")
+    
+    user.boards.create(title: Faker::Lorem.word, content: Faker::Lorem.paragraph)
+end
+
+Board.all.each do |board|
+    3.times do
+        board.comments.create(body: Faker::Kpop.iii_groups, user: User.first)
+    end
+end
